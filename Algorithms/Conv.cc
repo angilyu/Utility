6b72b842d547729d8b1d11da5dad857d58e9cbfc
Wenjing Yu <angilyu@gmail.com>
2012-06-06 00:52:03 -0400

    add toDouble

--- Algorithms/Conv.cc
+++ Algorithms/Conv.cc
@@ -20,6 +20,32 @@ int toInt(const char* str) {
     }
     return negative ? -sum : sum;
 }
+double toDouble(const char* str) {
+    bool isNegative = false;
+    if (*str == '-' || *str == '+') {
+        isNegative = *str == '-';
+        str++;
+    }
 
+    int numerator = 1;
+    // dotFound indicates if we've found the '.'
+    bool dotFound = false;
+    double sum = 0;
+
+    while (*str != '\0') {
+        if (*str == '.') {
+            // '.' shouldn't appear more than once
+            assert(!dotFound);
+            dotFound = true;
+        } else {
+            if (dotFound) {
+                numerator *= 10;
+            }
+            sum = sum * 10 + *str - '0';
+        }
+        ++str;
+    }
+    return isNegative ? -(sum / numerator) : (sum / numerator);
+}
 } // end namespace util
 } // end namespace wenjing
--- Algorithms/Conv.h
+++ Algorithms/Conv.h
@@ -5,9 +5,15 @@ namespace wenjing{
 namespace util{
 /// toInt convert string to int
 ///
-/// @param string should be made of digit
+/// @param string should be made of digit, '-' and '+'
 /// @return integer value
 int toInt(const char* str);
+
+/// toDouble convert string to double
+/// 
+/// @param string should be made of digit, dot, '-' and '+'
+/// @return double value
+double toDouble(const char* str);
 
 } // end namespace util
 } // end namespace wenjing
