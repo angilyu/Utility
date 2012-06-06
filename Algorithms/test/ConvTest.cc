acfa8af978e78c18268f59071ae0491b43119d10
Wenjing Yu <angilyu@gmail.com>
2012-06-05 23:33:16 -0400

    add more test cases for toInt()

--- Algorithms/test/ConvTest.cc
+++ Algorithms/test/ConvTest.cc
@@ -7,9 +7,9 @@ TEST(ToInt, Positive) {
     ASSERT_EQ(123, toInt("123"));
 }
 TEST(ToInt, PositiveWithSign) {
-
+    ASSERT_EQ(123, toInt("+123"));
 }
 TEST(ToInt, Negative) {
-
+    ASSERT_EQ(-12, toInt("-12"));
 }
 #include <TestRunner.cc>
